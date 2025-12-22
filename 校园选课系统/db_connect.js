debff3785f03d41a2cee035f6422c869b0b58587
CTX401 <txchai17@gmail.com>
2025-12-22 10:34:32 +0800

    deleted:    h.txt 	modified:   "\346\240\241\345\233\255\351\200\211\350\257\276\347\263\273\347\273\237/db_connect.js" 	deleted:    "\346\240\241\345\233\255\351\200\211\350\257\276\347\263\273\347\273\237/main/PHP/test.php" 	deleted:    "\346\240\241\345\233\255\351\200\211\350\257\276\347\263\273\347\273\237/main/python/login_python.py" 	deleted:    "\346\240\241\345\233\255\351\200\211\350\257\276\347\263\273\347\273\237/main/python/test.py"

--- 校园选课系统/db_connect.js
+++ 校园选课系统/db_connect.js
@@ -28,7 +28,6 @@ app.get("/", (_, res) => {
 
 app.post("/submit", (req, res) =>{
     const {user, password} = req.body;
-
     const sql = "SELECT * FROM login_info WHERE id = ?";
     
     db.query(sql, [user], (err, results)=>{
@@ -43,13 +42,12 @@ app.post("/submit", (req, res) =>{
                 console.log("登陆成功");
                 console.log(db_user.id);
                 if(db_user.user === "admin"){      
-
                     return res.json({success:true, redirect:"../mainAdmin.html", username:db_user.user});
                 }else{
                     if(db_user.id[0] == "T"){
                         return res.json({success:true, redirect:"../mainTeacher.html", username:db_user.user});    
                     }else{
-                        return res.json({success:true, redirect:"../main.html", username:db_user.user});
+                        return res.json({success:true, redirect:"../mainStudent.html", username:db_user.user});
                     }                
                 }
             }else{
@@ -61,7 +59,7 @@ app.post("/submit", (req, res) =>{
 
 // 主页(admin)
 // 获取学生的用户信息
-app.get("/api/adminStudentData", (req, resStd) => {
+app.get("/api/admin/StudentData", (_, resStd) => {
     const stdSql = "SELECT * FROM student_info";
 
     db.query(stdSql, (err, results) => {
@@ -74,7 +72,7 @@ app.get("/api/adminStudentData", (req, resStd) => {
 });
 
 //获取教师的用户信息
-app.get("/api/adminTeacherData", (req, resTch) => {
+app.get("/api/admin/TeacherData", (_, resTch) => {
     const tchSql = "SELECT * FROM teacher_info";
 
     db.query(tchSql, (err, results) => {
@@ -86,6 +84,19 @@ app.get("/api/adminTeacherData", (req, resTch) => {
     });
 });
 
+//获取登录信息
+app.get("/api/admin/LoginData", (_, resLog) => {
+    const logSql = "SELECT * FROM login_info";
+
+    db.query(logSql, (err, results) => {
+        if(err){
+            console.err("查询错误 ：", err);
+            return resLog.status(500).json({success: false, message:"数据库查询失败"});
+        }
+        resLog.json({success: true, data: results});
+    });
+});
+
 //激活用户
 app.post("/api/userActivate", (req, res)=>{
     const {userId, oldPass, newPass} = req.body;
@@ -93,16 +104,16 @@ app.post("/api/userActivate", (req, res)=>{
     const actUser = "UPDATE login_info SET password=? , active='1' WHERE id=?";
 
     db.query(checkPass, [userId, oldPass], (err, result)=>{
-        console.log(result);
+        // console.log(result);
         if(err){
-            return res.json({success:false, message:"数据库错误"});
+            return resLog.status(500).json({success:false, message:"数据库错误"});
         }
         if(result.length == 0){
             return res.json({success: false, message: "用户或密码错误"});
         }
         db.query(actUser, [newPass, userId], (err, result)=>{
             if(err){
-                return res.json({success:false, message:"数据库错误"});
+                return resLog.status(500).json({success:false, message:"数据库错误"});
             }
             res.json({success: true, message:"账号成功激活"});
         });
@@ -136,16 +147,154 @@ app.post("/api/dltUser", (req, res)=>{
 
 //修改用户数据
 app.post("/api/editUser", (req, res) =>{
-    const sql = req.body;
-
-    db.query(sql, (err, result)=>{
+    console.log(req.body.sql);
+    db.query(req.body.sql, (err, result)=>{        
         if(err){
             return res.json({success: false, message: "修改失败"});
         }
-        res.json({success: true, message: "修改成功"});
+        res.json({success: true, message: "成功修改"});
     });
 });
 
+//添加用户
+app.post("/api/addUser", (req, res) => {
+    db.query(req.body.sql, (err, result)=>{
+        if(err) return res.json({success: false, message: "添加失败"});
+
+        db.query(req.body.logSql, (err, results) => {
+            if(err) return res.json({success: false, message: "添加失败"});        
+            res.json({success: true, message: "成功添加"});
+        });       
+    });
+});
+
+// 获取课程信息
+// const courseDict = {};
+
+app.get("/api/course/info", (req, res) => {
+    const search = req.query.search || "";
+    const courseInfoSql = "SELECT * FROM teacher_course WHERE course_name LIKE ? OR name LIKE ?";     
+    const courseInfo = {};
+    const course_name = new Set();
+    const codes = new Set();
+    const department = new Set();
+
+    db.query(courseInfoSql, [`%${search}%`, `%${search}%`], (err, results) => {  
+        if(err) return res.status(500).json({error: err, message: "无法获取课程数据"});  
+
+        results.forEach(name => {
+            course_name.add(name.course_name);
+        });
+
+        results.forEach(row => {
+            codes.add(row.course_code);
+        });
+
+        codes.forEach(code => {
+            courseInfo[code] = new Array();
+        });
+
+        results.forEach(row => {   
+            courseInfo[row.course_code].push({
+                teacher: row.name,
+                course_name: row.course_name,
+                course_code: row.course_code,
+                department: row.department,
+                time: row.time,
+                classroom: row.classroom, 
+                credit: row.credit,         
+                current_student: row.current_student,
+                max_student: row.max_student,
+                day: row.day
+            });
+            department.add(row.department);
+        });
+        // console.log(courseInfo);
+        res.json({success: true, data: courseInfo, department: Array.from(department), course_name: Array.from(course_name)});
+    });       
+});
+
+app.post("/api/selection/select", async (req, res) => {
+    const select = req.body.selectedCourse;
+    const studentId = req.body.studentId;
+    const DATETIME = new Date();
+
+    try {
+        const validity = await validateSelection(Id=studentId, sessionTime=select.time, sessionDay=select.day, max_capacity=select.max_student, current_capacity=select.current_student);
+
+        if(!validity.success){
+            return res.json(validity);
+        }
+
+        const sql = "INSERT INTO student_course (student_id, course_code, select_time, teacher, status) VALUES (?, ?, ?, ?, ?)";        
+
+        db.query(sql, [studentId, select.course_code, DATETIME, select.teacher, "已选"], (err) => {
+            if(err) return res.json({success: false, message: err});
+            res.json({success: true, message: validity.message});
+        });
+    }catch(err){
+        res.status(500).json({success: false, message: err.message});
+    }
+});
+
+async function validateSelection(Id, sessionTime, sessionDay, max_capacity, current_capacity){    
+    
+    if(Number(max_capacity) - Number(current_capacity) == 0){
+        return {success: false, message: "课程已满"};
+    }
+
+    const stdClassSql = "SELECT * FROM student_course WHERE student_id = ?";
+    const results = await queryValSelect(stdClassSql, [Id]);
+
+    const TIME = {};
+
+    for(const row of results){
+        const tcSql = "SELECT course_name, time, day FROM teacher_course WHERE course_code = ? AND name = ?";
+        const tc = await queryValSelect(tcSql, [row.course_code, row.teacher]);
+
+        if(tc.length > 0){
+            TIME[tc[0].course_name] = `${tc[0].day}${tc[0].time}`;
+        }
+    }
+
+    for(const [courseName, T] of Object.entries(TIME)){
+        if(T[0] === sessionTime){
+            if(T === sessionTime){
+                return {success: false, message: `与 ${courseName} 的时间冲突`};
+            }
+
+            if(T[0] === sessionTime[0] && T[1] === "A"){
+                return {success: false, message: `与 ${courseName} 的时间冲突`};
+            }
+        }
+    }
+    return {success: true, message: "选课成功"};
+};
+
+
+
+app.get("/api/course/schedule/:id", async (req, res) => {
+    const schedSql = "SELECT * FROM student_course WHERE student_id = ?";    
+    const userID = req.params.id;
+    const results = await queryValSelect(schedSql, [userID]);
+    const schedule = new Array();
+
+    for(const row of results){
+        const tcSql = "SELECT * FROM teacher_course WHERE course_code = ? AND name = ?";
+        const tc = await queryValSelect(tcSql, [row.course_code, row.teacher]);        
+        schedule.push(tc[0]);
+    }    
+    res.json({success: true, data: schedule});
+});
+
+function queryValSelect(sql, params=[]){
+    return new Promise((resolve, reject) => {
+        db.query(sql, params ,(err, results) => {
+            if(err) reject(err);
+            else resolve(results);
+        });
+    });
+}
 app.listen(3000, () => {
   console.log("🚀 Server running on http://localhost:3000");
 });
