613091e8b68289138058338789a4df838ccda440
CTX401 <txchai17@gmail.com>
2026-01-11 13:51:35 +0800

    update 校园选课系统 content

--- 校园选课系统/main/scripts/main.js
+++
@@ -1,6 +0,0 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const userName = document.getElementById("userName");
-    const userGet = localStorage.getItem('username');
-
-    userName.textContent = userGet ? userGet : "12345678901234567890";
-});
